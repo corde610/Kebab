4cb884c4b8545b679da7516b8bd0a5f07b85e987
Victor Cordero <vcorderovillaverde@gmail.com>
2025-10-22 00:46:13 +0200

    ahora si que te echa la bronca si lo pides antes por gordo

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 const KEBABTELEFONO = 623390199;
 $comerKebab = true;
-date_default_timezone_set ("Europe/Madrid");
+date_default_timezone_set("Europe/Madrid");
 $tz = date_default_timezone_get();
 $time = date("H:i");
 
 echo "<h1> ¿Sale Shawarma? </h1>";
 echo "<h2> Son las $time </h2> <br/>";
+//se supone que iba a usar el php para coger la hora y que no pidieras un kebab antes de las 20:00
 ?>
 
 <button onclick="alertaKebab()"> Pulsame gilipollas </button>
 <img src="https://thumbs.dreamstime.com/b/kebab-mascot-character-illustration-healthy-looking-cartoon-souvlaki-masacot-man-59479600.jpg" alt="Kebab Mascot" width="300" height="300">
+<div id="comer"></div>
 
 <head>
     <title> Debería? </title>
@@ -20,16 +22,22 @@ echo "<h2> Son las $time </h2> <br/>";
 </head>
 
 <script>
-    let comerKebab = () => Math.random() >= 0.5 ; // aqui la probabilidad meu
+    let comerKebab = () => Math.random() >= 0.5; // aqui la probabilidad meu
     comerKebab(); // true
+    let mensaje = document.getElementById("comer"); //aqui el mensajito debajo de la foto
+    const hora = "<?php echo $time ?>";
 
- function alertaKebab() {
-    if(comerKebab()==true) {
-       alert(<?php echo KEBABTELEFONO; ?>);
-    }
-    else {
-       alert("Hoxe non toca kebab meu");
-    }
+
+    function alertaKebab() {
+         if(hora < "20:00" && hora > "23:00") {
+            mensaje.innerHTML = 'Es moi cedo para pensar en kebabs neno'
+        }
+        else if (comerKebab() == true)  {
+            mensaje.innerHTML = '<a href="tel:+34623390199"> 623 390 199 </a>'
+            
+        } else {
+            mensaje.innerHTML = 'No meu, hoxe non toca kebab'
+        }
     }
 </script>
 
@@ -38,6 +46,12 @@ echo "<h2> Son las $time </h2> <br/>";
 <style>
     :root {
         color-scheme: light dark;
+    }
+
+    #comer {
+        margin-top: 30px;
+        font-size: 4em;
+        text-align: center;
     }
 
     img {
@@ -49,7 +63,7 @@ echo "<h2> Son las $time </h2> <br/>";
 
     body {
         font-family: Arial, sans-serif;
-        margin: 20px;
+        margin: 10px;
         text-align: center;
         display: grid;
         place-content: center;
